9a9f0601b8606a2af1e4509d178a91a0a70644e6
stonkle <asievert07@gmail.com>
2025-03-29 16:39:29 -0500

    Updated About Section

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -2,7 +2,8 @@ import { RevealOnScroll } from "./RevealOnScroll";
 
 export const About = () => {
     const frontendSkills = ["React", "TailwindCSS", "JavaScript", "React Native"];
-    const gamedevSkills = ["Unity", "C#", "Gamemaker", "GML"]
+    const gamedevSkills = ["Unity", "C#", "Gamemaker", "GML"];
+    const backendSkills = ["MySQL", "Docker", "ExpressJS", "Node.js"];
 
     return (
         <section id="about" className="min-h-screen flex items-center justify-center py-20">
@@ -41,6 +42,18 @@ export const About = () => {
                                 ))}
                             </div>
                         </div>
+
+                        <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                            <h3 className="text-xl font-bold mb-4">Backend</h3>
+                            <div className="flex flex-wrap gap-2">
+                                {backendSkills.map((tech, key) => (
+                                    <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm 
+                                                    hover:bg-blue-500/20 hover:shadow-[0_2px-8px_rgba(59,130,246,0.2)] transition">
+                                        {tech}
+                                    </span>
+                                ))}
+                            </div>
+                        </div>
                     </div>
                 </div>
 
@@ -60,13 +73,8 @@ export const About = () => {
                         <h3 className="text-xl font-bold mb-4"> 💼 Work Experience </h3>
                         <div className="space-y-4 text-gray-300">
                             <div>
-                                <h4 className="font-semibold">Job Name at Company</h4>
-                                <p> Job description </p>
-                            </div>
-
-                            <div>
-                                <h4 className="font-semibold">Job Name at Company</h4>
-                                <p> Job description </p>
+                                <h4 className="font-semibold">Gameplay Systems Engineer Intern at Playing For Real (2024) - </h4>
+                                <p> Connected complex systems and programmed clean UI functionality to effectively display information to players. </p>
                             </div>
                         </div>
                     </div>
